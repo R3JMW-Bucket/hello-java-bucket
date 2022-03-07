cc536cd75ce10316c854baaf1fa5e42ffbfc3c34
Dave <dave.ran.jin@kingland.com>
2022-03-07 17:11:55 +0800

    init

---
+++ src/main/java/com/helloj/test/TestSprintbootFeatures.java
@@ -0,0 +1,13 @@
+package com.helloj.test;
+
+public class TestSprintbootFeatures {
+
+    public void getId(Long proposalId) {
+
+    }
+
+
+    public void methodInvoke() {
+        getId(null);
+    }
+}
